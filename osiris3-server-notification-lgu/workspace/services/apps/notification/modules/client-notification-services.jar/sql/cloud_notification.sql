c873a7179d3888f404606c8dc320e4ba491572ce
worgflores <worgflores@gmail.com>
2014-05-16 15:04:08 +0800

    added osiris3-server-notification-lgu

---
+++ osiris3-server-notification-lgu/workspace/services/apps/notification/modules/client-notification-services.jar/sql/cloud_notification.sql
@@ -0,0 +1,28 @@
+[getList]
+SELECT * FROM cloud_notification WHERE groupid=$P{groupid} ORDER BY dtfiled 
+
+[getAttachments]
+SELECT * FROM cloud_notification_attachment 
+WHERE parentid=${objid} 
+ORDER BY indexno 
+
+[getPendingMessages]
+SELECT * FROM cloud_notification_pending ORDER BY dtfiled 
+
+[reschedulePending]
+UPDATE cloud_notification_pending SET 
+	dtretry=$P{dtretry} 
+WHERE 
+	objid=$P{objid} 
+
+[removePending]
+DELETE FROM cloud_notification_pending WHERE objid=$P{objid} 
+
+[removeFailed]
+DELETE FROM cloud_notification_failed WHERE refid=$P{refid} 
+
+[removeDelivered]
+DELETE FROM cloud_notification_delivered WHERE objid=$P{objid} 
+
+[removeReceived]
+DELETE FROM cloud_notification_received WHERE objid=$P{objid} 
